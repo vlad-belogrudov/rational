3f12a9cbd67dc26f9204e115cecb646a2b790703
Vladislav Belogrudov <v.belogrudov@yadro.com>
2022-07-06 11:11:43 +0300

    use ddt for testing divisor

--- tests/test_gcd.py
+++ tests/test_gcd.py
@@ -1,22 +1,21 @@
 import unittest
 
+from ddt import data, ddt, unpack
+
 from rational.gcd import gcd
 
-
+@ddt
 class TestGCD(unittest.TestCase):
 
-    def test_divisor(self):
-        numbers = {
-            (0, 0): 1,
-            (8, 0): 8,
-            (0, 8): 8,
-            (-1, 2): 1,
-            (-8, 16): 8,
-            (16, -8): 8,
-            (36, 48): 12,
-            (12345, 54321): 3,
-        }
-
-        for p, r in numbers.items():
-            with self.subTest(msg=f"{p} => {r}"):
-                self.assertEqual(gcd(*p), r)
+    @data((0, 0, 1),
+          (8, 0, 8),
+          (0, 8, 8),
+          (-1, 2, 1),
+          (-8, 16, 8),
+          (16, -8, 8),
+          (36, 48, 12),
+          (12345, 54321, 3),
+    )
+    @unpack
+    def test_divisor(self, first, second, divisor):
+        self.assertEqual(gcd(first, second), divisor)
